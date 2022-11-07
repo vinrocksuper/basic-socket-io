86b6eb5c5819736eec8d17fbe1b75edc0108fc25
Austin Willoughby <arwigm@rit.edu>
2022-11-07 11:01:53 -0500

    Starter code

---
+++ server/router.js
@@ -0,0 +1,8 @@
+const controllers = require('./controllers');
+
+const router = (app) => {
+  app.get('/', controllers.index);
+  app.get('/*', controllers.notFound);
+};
+
+module.exports = router;
